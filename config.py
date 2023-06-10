58dd9529897e4cae75ef0a68507da24f127b7b95
erenali22 <erena.li@outlook.com>
2023-06-10 06:07:00 -0700

    fix2

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
-DATABASE_URI = 'sqlite:///chitter.db'
-SECRET_KEY = 'asdjasd02309jDs'
+# DATABASE_URI = 'sqlite:///chitter.db'
+# SECRET_KEY = 'asdjasd02309jDs'
 
 
-# DATABASE_URI = os.environ.get(
-#         'DATABASE_URL').replace('postgres://', 'postgresql://')
+DATABASE_URI = os.environ.get(
+        'DATABASE_URL').replace('postgres://', 'postgresql://')
 
-# SECRET_KEY = os.environ.get(
-#         'SECRET_KEY')
+SECRET_KEY = os.environ.get(
+        'SECRET_KEY')
